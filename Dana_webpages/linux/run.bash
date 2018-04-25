b011858085726977cf65aa19759145e1f88f8f82
sasadla <saarimaatri@hotmail.com>
2018-04-25 13:51:14 +0100

    Added docker compose file to create docker container with webpages in them

---
+++ Dana_webpages/linux/run.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#ls list directories
+#env list all enviornment variables
+
+dnc web.dn
+dnc text_page.dn
+dnc image_page.dn
+dnc image_text_page.dn
+echo "Dana Component running on "
+hostname --ip-address 
+echo " ...."
+dana ws.core
