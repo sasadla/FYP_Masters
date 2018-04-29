02a5e1b07efcd6b5d953481231f29882778377f0
sasadla <saarimaatri@hotmail.com>
2018-04-29 16:02:37 +0100

    more improvements added to the code and finalized the basic  architecture ready for testing form here on out some minor adjustments  still need to be made to the ext.dn code

--- Docker_dana_server/live_container_info.bat
+++ Docker_dana_server/live_container_info.bat
@@ -2,8 +2,10 @@ if exist status_res.txt break> status_res.txt
 echo My Identity is: >> status_res.txt
 hostname >> status_res.txt
 ::hostname --ip-address >> alive_ids.txt :: for linux machines to get ip
-::ipconfig | findstr /R /C:"IPv4 Address" >> alive_ids.txt  for ipv4 list in windows
+::ipconfig | findstr /R /C:"IPv4 Address" >> alive_ids.txt :: for ipv4 list in windows
 echo And im running following conainer ids: >> status_res.txt
 docker ps -q -a >> status_res.txt
+::echo  >>status_res.txt
+echo _ >>status_res.txt
 echo docker status is: >>status_res.txt
 docker ps >> status_res.txt
