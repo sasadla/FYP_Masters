688a82f192a74bf7ad947166933e2458fbf423ec
sasadla <saarimaatri@hotmail.com>
2018-05-02 21:02:27 +0100

    finalized directory to update to master

--- Docker_dana_server/down_script.bat
+++ Docker_dana_server/down_script.bat
@@ -1,3 +1,4 @@
+cd D:\MSCI\Fyp_masters\Fyp_code\FYP_Masters\Docker_dana_server\
 if exist down_res.txt break> down_res.txt
 docker-compose down >> down_res.txt
 echo d_down_success >> down_res.txt
--- Docker_dana_server/up_script.bat
+++ Docker_dana_server/up_script.bat
@@ -1,3 +1,4 @@
+cd D:\MSCI\Fyp_masters\Fyp_code\FYP_Masters\Docker_dana_server\
 if exist up_res.txt break> up_res.txt
 if exist down_res.txt break> down_res.txt
 docker-compose build
