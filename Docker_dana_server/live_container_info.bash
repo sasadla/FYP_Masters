e2ce58102d2b8ad486b98f6e90e463cde797ffab
sasadla <saarimaatri@hotmail.com>
2018-05-23 13:57:57 +0100

    edited all scripts and fixed small issue with them to make sure desired results are achieved

--- Docker_dana_server/live_container_info.bash
+++ Docker_dana_server/live_container_info.bash
@@ -6,7 +6,7 @@ then
 fi
 
 echo My Identity is: >> ./status_res.txt
-hostname --ip-address >> ./status_res.txt
+hostname >> ./status_res.txt
 echo And im running following conainer ids: >> ./status_res.txt
 docker ps -q -a >> ./status_res.txt
 echo _ >>./status_res.txt
